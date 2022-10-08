d334c5a2b3ae342ad20fec9526d2a286187531d5
asheessharma <80744753+asheessharma@users.noreply.github.com>
2022-10-08 22:09:57 +0530

    Add files via upload

---
+++ midterm1.php
@@ -0,0 +1,93 @@
+<?php
+   // $str="What a beautiful day it is !!!";
+   //print_r(count_chars($str,1));
+   // echo(chunk_split($str,2,":"));
+   // $res=print(15>8)? printf("%d",printf("great learning")): "no" ;
+   // echo $res;
+   //$arr=array(1,2,3,4,'a','A');
+   //foreach($arr as $i){
+   //     echo("$i <br>");
+    //}
+        
+   /* declare(strict_types=1);
+    function Teacher(string $name)
+    {
+        $index = 0; 
+        for (; $index <= 4; $index++) {
+            echo strrev($name)."<br>";
+            $index++;
+            }
+            $index++;
+    }
+    Teacher("PHP class");*/
+  /*  function greatest($num1,$num2,$num3){
+        switch($num1>$num2 && $num1>$num3){
+            case 1:
+                echo "$num1 is greatest";
+                break;
+            case 0:
+                switch($num2>$num1 && $num2>$num3){
+                    case 1:
+                        echo "$num2 is greatest";
+                        break;
+                    case 0:
+                        echo "$num3 is greatest";
+                        break;
+                }
+    
+        }
+    }
+    greatest(2,3,0);*/
+   /* $res=0;
+    $res=sprintf(printf("%x",printf("PHP examination")));
+    echo $res;*/
+   /* $arr1=range(1,5);
+    foreach($arr1 as $key=>$values){
+        echo "arr1[".$key."]=".$values."<br>";
+    }*/
+    //$my_array = array("Dog","Cat","Horse","Fish","Frog");
+    //list($a, $b, $c) = $my_array;
+    //echo "I have several animals, a $a, a $b and a $c.";
+   /* $key=implode(array_keys($my_array,"Fish"));
+    print_r($my_array);
+    echo"<br>";
+    unset($my_array[(int)$key]);
+    print_r($my_array);*/
+   //$str6='1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
+  //  echo(substr(str_shuffle($str6),0,7));
+  /* $a="Ashish";
+   $b="ashish";
+   $c=4;
+   $d="4";
+    var_dump($a==$b );
+   echo "<br>";
+    var_dump($c==$d );
+   echo "<br>";
+   var_dump ($a===$b );
+   echo "<br>";
+   var_dump ($c===$d );
+   echo "<br>";
+   var_dump($a>=$b );
+   echo "<br>";
+   var_dump($c>=$d );
+   echo "<br>";
+   var_dump($a<>$b );
+   echo "<br>";
+    var_dump($c<>$d );
+   echo "<br>";
+   var_dump($c<=>$d);
+   echo "<br>";*/
+   $m=null;
+   $n="50";
+    if($m??$n)
+    {
+
+    printf("%o",$m=11);
+
+    }
+    else{
+    echo $n;
+}
+
+
+?>
